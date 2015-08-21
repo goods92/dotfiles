5cc74f35792b675a41bdf502da4caa6487474b83
Sam Goodchild <sgoodchild11692@gmail.com>
2015-08-21 14:07:49 -0500

    Fixed packages and brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Need to write a for loop that will look through all of the packages in a list that I provide and will loop throug to ensure that (which _) is false
 
 if test ! $(which brew); then
     echo "Installing homebrew"

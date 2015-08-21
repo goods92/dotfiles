0069d9f0cc3fed73515e2bd5ed8bd350d25a6553
Sam Goodchild <sgoodchild11692@gmail.com>
2015-08-21 12:53:16 -0500

    Fixed issue where it was calling the wrong file name

--- install/brew.sh
+++ install/brew.sh
@@ -8,28 +8,28 @@ fi
 echo "Installing homebrew packages..."
 
 # cli tools
-brew install ack
-brew install tree
-brew install wget
+# brew install ack
+# brew install tree
+# brew install wget
 
 # development server setup
-brew install nginx
-brew install dnsmasq
+# brew install nginx
+# brew install dnsmasq
 
 # development tools
-brew install git
-brew install hub
-brew install macvim --override-system-vim
-brew install reattach-to-user-namespace
-brew install tmux
-brew install zsh
-brew install highlight
-brew install nvm
-brew install z
-brew install markdown
+# brew install git
+# brew install hub
+# brew install macvim --override-system-vim
+# brew install reattach-to-user-namespace
+# brew install tmux
+# brew install zsh
+# brew install highlight
+# brew install nvm
+# brew install z
+# brew install markdown
 
 # install neovim
-brew tap neovim/neovim
-brew install --HEAD neovim
+# brew tap neovim/neovim
+# brew install --HEAD neovim
 
 exit 0
--- install/link.sh
+++ install/link.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-DOTFILES=$HOME/.dotfiles
-
+#DOTFILES=$HOME/.dotfiles
+DOTFILES=/Users/sgoodchild11692/Documents/Programming/dotfiles
 echo "creating symlinks"
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
-for file in $linkables ; do
-    target="$HOME/.$( basename $file ".symlink" )"
-    echo "creating symlink for $file"
-    ln -s $DOTFILES/$file $target
+for file_temp in $linkables ; do
+    target="$HOME/.$( basename $file_temp ".symlink" )"
+    echo "creating symlink for $file_temp"
+    ln -s $file_temp $target
+    #ln -s $DOTFILES/$file_temp $target
 done

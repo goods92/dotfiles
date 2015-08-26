631f260e4c8ec8a083f0d69ee63d514e3bbc6509
Sam Goodchild <sgoodchild11692@gmail.com>
2015-08-25 20:41:02 -0500

    Created a to do file

---
+++ TODO.md
@@ -0,0 +1,3 @@
+To do:
+
+learn Docker, and then node with Docker
--- installosx.sh
+++ installosx.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env sh
+
+echo "Show Path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true
+

33fab8d71e786eebc55697d1503bc6ee9db2f089
samgoodchild <sgoodchild11692@gmail.com>
2015-09-16 22:20:43 -0500

    Add update to man command

--- zsh/helpers/zsh.zsh
+++ zsh/helpers/zsh.zsh
@@ -35,6 +35,17 @@ function pd()
     pushd $choice
 }
 
+man() {
+      env \
+      LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+      LESS_TERMCAP_md=$(printf "\e[1;31m") \
+      LESS_TERMCAP_me=$(printf "\e[0m") \
+      LESS_TERMCAP_se=$(printf "\e[0m") \
+      LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+      LESS_TERMCAP_ue=$(printf "\e[0m") \
+      LESS_TERMCAP_us=$(printf "\e[1;32m") \
+      man "$@"
+}
 # Uncomment following line if you want red dots to be displayed while waiting for completion
 # COMPLETION_WAITING_DOTS="true"
 

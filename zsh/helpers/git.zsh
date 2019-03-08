64340a846cd7db9515ad4561cdaafd3944164e9d
Sam Goodchild <sgoodchild11692@gmail.com>
2019-03-08 09:27:03 -0500

    Removed vim symlinks to completely switch to neovim

--- zsh/helpers/git.zsh
+++ zsh/helpers/git.zsh
@@ -1,2 +1,3 @@
 alias 'g'="git"
 alias 'gs'='git status'
+alias 'gl'='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
